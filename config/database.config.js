828e4443bc5f02f759fa098ec72b95c4d27ebf2a
Emmanuel <ei.watila@gmail.com>
2021-09-09 17:00:57 +0100

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   url:
-    "mongodb+srv://<username>:<password>@cluster0.nvfrf.gcp.mongodb.net/devsgnr?retryWrites=true&w=majority",
+    "mongodb+srv://<username>:<password>@cluster0.nvfrf.gcp.mongodb.net/<db_name>?retryWrites=true&w=majority",
 };
